0a29ea6f329af28967d5d9a77254bf14ccfa62d7
Juan L <33398520+JuanLucasLaur@users.noreply.github.com>
2022-01-15 20:36:41 -0300

    Allow clients to retrieve their money

--- Auction.sol
+++ Auction.sol
@@ -18,6 +18,7 @@ contract Auction {
     State public auctionState;
 
     mapping(address => uint256) public bids;
+    mapping(address => bool) public payedBidders;
 
     constructor() {
         owner = payable(msg.sender);
@@ -35,8 +36,8 @@ contract Auction {
         /// @dev Don't let owner bid so that it can't artificially increase the price.
         require(msg.sender != owner);
         /// @dev Check that the auction is active.
-        require(block.number >= startTime, "Auction isn't active");
-        require(block.number <= endTime, "Auction isn't active");
+        require(block.timestamp >= startTime, "Auction isn't active");
+        require(block.timestamp <= endTime, "Auction isn't active");
         require(auctionState == State.Running, "Auction isn't active");
 
         require(msg.value >= bidIncrement, "Bid too small");
@@ -60,6 +61,44 @@ contract Auction {
     function cancelAuction() public {
         require(msg.sender == owner);
         auctionState = State.Cancelled;
+    }
+
+    /**
+     * @notice Allows bidders to retrieve their money if the auction finishes or is cancelled. Only the owner or bidders can do this.
+     */
+    function afterAuction() public {
+        require(auctionState == State.Cancelled || block.timestamp > endTime, "Auction not over nor cancelled");
+        require(payedBidders[msg.sender] == false, "Funds already claimed");
+        require(msg.sender == owner || bids[msg.sender] > 0, "Not a participating address");
+
+        address payable recipient;
+        uint256 value;
+
+        if (auctionState == State.Cancelled) {
+            // The auction was cancelled and bidders can retrieve their money.
+            recipient = payable(msg.sender);
+            value = bids[msg.sender];
+        } else {
+            // The auction ended.
+            if (msg.sender == owner) {
+                // The owner can retrieve the money from the sale.
+                recipient = owner;
+                value = highestBindingBid;
+            } else {
+                if (msg.sender == highestBidder) {
+                    // The highest bidder can retrieve the difference between the value he bid and the highestBindingBid.
+                    recipient = highestBidder;
+                    value = bids[highestBidder] - highestBindingBid;
+                } else {
+                    // Bidders that didn't win the auction can retrieve their funds.
+                    recipient = payable(msg.sender);
+                    value = bids[msg.sender];
+                }
+            }
+        }
+
+        payedBidders[msg.sender] = true;
+        recipient.transfer(value);
     }
 
     /**
